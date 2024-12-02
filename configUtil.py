6a8a090fe72266ad2479f790058c35be6359366a
treysius <treysius@gmail.com>
2024-12-01 22:30:25 -0500

    Add files via upload

---
+++ configUtil.py
@@ -0,0 +1,21 @@
+import configparser
+import os
+
+
+def load_config(filename: "") -> configparser.ConfigParser:
+    c = configparser.ConfigParser()
+    # if config does not exist, create default config
+    if not os.path.isfile(filename):
+        portNum = 7895
+
+        c['Connections'] = {'server_port': portNum}
+        c['Database'] = {'db_filename': 'database.db', 'audio_dirname': 'audio'}
+
+        # save config file
+        with(open(filename, 'w')) as configFile:
+            c.write(configFile)
+
+    # if config does exist, load it
+    else:
+        c.read(filename)
+    return c
